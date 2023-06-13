5b6a1cf26c3bb07be7cff8f68442f467dc7235c2
cgsg189 <83028991+cgsgMB5@users.noreply.github.com>
2023-06-13 09:58:50 +0300

    Update frag.glsl

--- platon/frag.glsl
+++ platon/frag.glsl
@@ -13,7 +13,7 @@ uniform float ColorCoefR, ColorCoefG, ColorCoefB;
 void main() {
     //o_color = color;
     o_color = vec4(mix(vec3(ColorCoefR, ColorCoefG, ColorCoefB),
-        (draw_norm.xxx + draw_norm.yyy + draw_norm.zzz) / 1.5, 0.1), 1.0);
+        (draw_norm.xxx + draw_norm.yyy + draw_norm.zzz) / 1.5, 0.5), 1.0);
     //o_color = vec4(draw_pos, 1.0);//pos + vec3(1.0, 1.0, 1.0), 1.0);
     if (draw_pos.x == 0.0)
         o_color = vec4(1.0, 1.0, 1.0, 1.0);
